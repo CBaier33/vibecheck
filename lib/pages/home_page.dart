6e4fb68e2b6fba7e2716138ba9cbd8cbdb533d25
Christopher Baier <baier.tech@outlook.com>
2024-10-16 15:22:37 -0400

    working gradient display.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,18 +7,102 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Alignment> _topAlignmentAnimation;
+  late Animation<Alignment> _bottomAlignmentAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 8));
+
+    _topAlignmentAnimation = TweenSequence<Alignment>(
+      [
+        TweenSequenceItem<Alignment>(
+          tween: Tween<Alignment>(
+              begin: Alignment.topLeft, end: Alignment.topRight),
+          weight: 1,
+        ),
+        TweenSequenceItem<Alignment>(
+          tween: Tween<Alignment>(
+              begin: Alignment.topRight, end: Alignment.bottomRight),
+          weight: 1,
+        ),
+        TweenSequenceItem<Alignment>(
+          tween: Tween<Alignment>(
+              begin: Alignment.bottomRight, end: Alignment.bottomLeft),
+          weight: 1,
+        ),
+        TweenSequenceItem<Alignment>(
+          tween: Tween<Alignment>(
+              begin: Alignment.bottomLeft, end: Alignment.topLeft),
+          weight: 1,
+        ),
+      ],
+    ).animate(_controller);
+
+    _bottomAlignmentAnimation = TweenSequence<Alignment>(
+      [
+        TweenSequenceItem<Alignment>(
+          tween: Tween<Alignment>(
+              begin: Alignment.bottomRight, end: Alignment.bottomLeft),
+          weight: 1,
+        ),
+        TweenSequenceItem<Alignment>(
+          tween: Tween<Alignment>(
+              begin: Alignment.bottomLeft, end: Alignment.topLeft),
+          weight: 1,
+        ),
+        TweenSequenceItem<Alignment>(
+          tween: Tween<Alignment>(
+              begin: Alignment.topLeft, end: Alignment.topRight),
+          weight: 1,
+        ),
+        TweenSequenceItem<Alignment>(
+          tween: Tween<Alignment>(
+              begin: Alignment.topRight, end: Alignment.bottomRight),
+          weight: 1,
+        ),
+      ],
+    ).animate(_controller);
+
+    _controller.repeat();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
             child:
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('vibecheck')]
-            )
-        )
-    );
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      AnimatedBuilder(
+          animation: _controller,
+          builder: (BuildContext, context) {
+            return Container(
+                width: 300,
+                height: 300,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: const [
+                      Color(0xffef0940),
+                      Color(0xFF8F19EF),
+                     // Color(0xFF0827E8)
+                    ],
+                    begin: _topAlignmentAnimation.value,
+                    end: _bottomAlignmentAnimation.value,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ));
+          }),
+      const Text('vibecheck',
+        style: TextStyle(
+          fontSize: 40,
+          height: 2,
+          fontFamily: 'Georgia',
+        ))
+    ])));
   }
 }
